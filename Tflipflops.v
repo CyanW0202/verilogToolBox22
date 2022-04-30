e859258c89cd83beb3380ceebe6b75fd7349e669
CyanW0202 <47878471+CyanW0202@users.noreply.github.com>
2022-04-30 00:07:52 -0700

    Create Tflipflops.v

---
+++ Tflipflops.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: CPP
+// Engineer: Chia
+// 
+// Create Date: 04/29/2022 11:56:50 PM
+// Module Name: FFT
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FFT(
+    input T,
+    input clk,
+    output reg Q
+    );
+always@(negedge clk)
+begin
+    if(T)
+    Q = ~Q;
+    else
+    Q = Q;
+end
+endmodule
