bf33689aa3502183409f877b1030ac7e7caebca6
CyanW0202 <47878471+CyanW0202@users.noreply.github.com>
2022-05-03 00:52:16 -0700

    Update decoder.v

--- decoder.v
+++ decoder.v
@@ -1,1 +1,39 @@
 
+module Decoder12(
+  input A;
+  input e;
+  output reg D1;
+  output reg D2;
+);
+  always@(*)
+  begin
+    case([e,A])
+      2'b10: D0 = 1;
+      2'b11: D1 = 1;
+      default: D1 = 0; D0 = 0;
+    endcase
+  end
+endmodule
+
+//testbench
+
+module Decoder12_testbench();
+  reg A;
+  reg e;
+  wire D0, D1;
+  
+  Decoder12 dut(
+    .A(A),
+    .e(e),
+    .D1(D1),
+    .D0(D0)
+  );
+  
+  initial begin
+    e = 0; A = 0;
+  #80: e = 1; A = 0;
+  #80: e = 0; A = 1;
+  #80: e = 1; A = 1;
+  #80: e = 1; A = 0;
+  end
+endmodule  
