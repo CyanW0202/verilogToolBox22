fcdd019e20424c05197e46888275a1dd2242ae3f
ChiaW <47878471+CyanW0202@users.noreply.github.com>
2022-11-15 12:15:24 -0800

    Update proj_Alarm.v

--- proj_Alarm.v
+++ proj_Alarm.v
@@ -1,3 +1,7 @@
+set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { reset }]; #IO_L24N_T3_RS0_15 Sch=sw[0]
+set_property -dict { PACKAGE_PIN L16   IOSTANDARD LVCMOS33 } [get_ports { playSound }]; #IO_L3N_T0_DQS_EMCCLK_14 Sch=sw[1]
+
+
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
